2d0b5f3c0c75de4acacc7973bb82bac71b70f164
Ihor Leonov <ihorleonow@gmail.com>
2023-03-25 16:44:07 +0200

    Update App.js

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,12 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
 import { SharedLayout } from 'components/sharedLayout/SharedLayout';
+import NotFound from 'Pages/notFound/NotFound';
 import { lazy } from 'react';
 
 const Cast = lazy(() => import('components/cast/Cast'));
 const Home = lazy(() => import('Pages/home/Home'));
 const MovieDetails = lazy(() => import('Pages/movieDetails/MovieDetails'));
 const Movies = lazy(() => import('Pages/movies/Movies'));
-const NotFound = lazy(() => import('Pages/notFound/NotFound'));
 const Reviews = lazy(() => import('components/reviews/Reviews'));
 
 export const App = () => {
